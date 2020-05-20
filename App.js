4793bd33a852536d490bcc9cde83a23694596754
stevedpedersen <stevedpedersen@gmail.com>
2020-05-20 16:35:34 -0700

    important update

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={{ alignSelf: 'center', flex: 0 }}>
+        <Image
+          style={{ alignSelf: 'center' }}
+          source={require('./assets/images/dbutt.jpg')}
+          resizeMode="contain"
+        />
+      </View>
     </View>
   );
 }
